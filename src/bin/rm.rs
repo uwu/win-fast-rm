a6ccb147448b3e6f8393b67436a82c1690b13b6d
xirreal <me@xirreal.dev>
2024-02-11 02:46:07 +0100

    Add deleted size data to logging

--- src/bin/rm.rs
+++ src/bin/rm.rs
@@ -1,4 +1,40 @@
+use std::path::PathBuf;
+
 use win_fast_rm::{delete_path, resolve_path};
+
+fn humanize_size(size: u64) -> String {
+    // Convert to nearest unit iteratively (KB, MB, GB, TB)
+    let mut size = size as f64;
+    for unit in [" bytes", " KB", " MB", " GB", " TB"].iter() {
+        if size < 1024.0 {
+            return format!("{:.2}{}", size, unit);
+        }
+        size /= 1024.0;
+    }
+
+    format!("{:.2}{}", size, " TB")
+}
+
+fn get_directory_size(path: &PathBuf) -> u64 {
+    let mut size = 0;
+
+    if !path.exists() {
+        return 0;
+    } else if path.is_file() {
+        return path.metadata().unwrap().len();
+    }
+
+    for entry in path.read_dir().unwrap() {
+        let entry = entry.unwrap();
+        let metadata = entry.metadata().unwrap();
+        if metadata.is_dir() {
+            size += get_directory_size(&entry.path());
+        } else {
+            size += metadata.len();
+        }
+    }
+    size
+}
 
 fn main() {
     #[cfg(not(windows))]
@@ -15,8 +51,10 @@ fn main() {
 
     let path = std::ffi::OsStr::new(&args[1]);
 
-    let start = std::time::Instant::now();
     let full_path = resolve_path(path);
+    let size = humanize_size(get_directory_size(&full_path));
+
+    let start = std::time::Instant::now();
 
     if let Err(e) = delete_path(&full_path) {
         let error_code = e.code().0;
@@ -31,8 +69,9 @@ fn main() {
     let duration = start.elapsed();
 
     println!(
-        "Deleted \"{}\" successfully in: {}ms",
+        "Deleted \"{}\"({}) successfully in: {}ms",
         resolve_path(path).display(),
+        size,
         duration.as_micros() as f64 / 1000.0
     );
 
